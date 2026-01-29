829083577e59c2d2a36fbb81d719b18750eeafcb
Sathish Shakthi <shakthisathish.developer@gmail.com>
2026-01-29 06:50:45 +0530

    UI Enhanced

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,10 +1,18 @@
 export default function Controls({ start, stop, status }) {
   return (
     <>
-      <button onClick={start} disabled={status !== "idle"}>
+      <button
+        className="aurora-btn"
+        onClick={start}
+        disabled={status !== "idle"}
+      >
         Start Conversation
       </button>
-      <button onClick={stop} disabled={status === "idle"}>
+      <button
+        className="aurora-btn"
+        onClick={stop}
+        disabled={status === "idle"}
+      >
         Stop Conversation
       </button>
     </>
